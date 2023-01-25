5d174f51995bf43af784e11cfad014eaf160d46f
Peter Woo <hw527@cam.ac.uk>
2023-01-25 08:09:43 +0000

    add: minimal working state for load cell measurements

--- lib/config/config.h
+++ lib/config/config.h
@@ -4,6 +4,11 @@
 #pragma once
 #include "inttypes.h"
 
+#include "pico/stdlib.h"
+#include "hardware/uart.h"
+#include "hardware/irq.h"
+
+
 // NOTE: The equivalent DSHOT speed is 8 Hz in DEBUG mode
 // This could be set here directly instead of changing:
 // DSHOT_PWM_WRAP, DSHOT_PWM_DIV, (DMA_ALARM_PERIOD)?
@@ -19,6 +24,12 @@
 #else
 #define MOTOR_GPIO 14
 #endif
+
+// Pin configurations
+constexpr uint LED_BUILTIN = 25;
+
+constexpr uint CLKPIN = 17;
+constexpr uint DATPIN = 16;
 
 // --- DMA Variables
 // Note that these should be cast uint32_t when sent to the slice
--- src/main.cpp
+++ src/main.cpp
@@ -62,8 +62,10 @@ void loop() {
             if (shoot::throttle_code == MAX_THROTTLE) {
                 printf("Max Throttle reached\n");
             } else {
-                shoot::throttle_code += 1;
-                printf("Throttle: %i", shoot::throttle_code);
+                shoot::throttle_code += 50;
+                if (shoot::throttle_code > MAX_THROTTLE)
+                    shoot::throttle_code = MAX_THROTTLE;
+                printf("Throttle: %i\n", shoot::throttle_code - ZERO_THROTTLE);
             }
         } else {
             printf("Motor is not in throttle mode\n");
@@ -77,8 +79,8 @@ void loop() {
             if (shoot::throttle_code == ZERO_THROTTLE) {
                 printf("Throttle is zero\n");
             } else {
-                shoot::throttle_code -= 1;
-                printf("Throttle: %i", shoot::throttle_code);
+                shoot::throttle_code -= 50;
+                printf("Throttle: %i\n", shoot::throttle_code - ZERO_THROTTLE);
             }
         } else {
             printf("Motor is not in throttle mode\n");
@@ -88,9 +90,10 @@ void loop() {
     // q - disarm
     // Not sure how to disarm yet. Maybe set throttle to 0 and don't send a
     // cmd for some secs?
-    if (strcmp(key_input, "q") == 0) {
+    if (key_input == 32) {
         shoot::throttle_code = ZERO_THROTTLE;
         shoot::telemetry = 0;
+        printf("Throttle: %i\n", 0);
     }
 
     // NOTE: In DEBUG mode, sending a DSHOT Frame takes a lot of time!
@@ -98,18 +101,37 @@ void loop() {
     // while sending commands!
     // But is this even needed?
     shoot::send_dshot_frame();
-
     printf("Finished processing byte.\n");
 }
 
 int main() {
+    int32_t thrust;
+    int key_input;
+
+    // Load cell calibration
+    int32_t zero_val = 150622;
+
     stdio_init_all();
 
     gpio_init(LED_BUILTIN);
     gpio_set_dir(LED_BUILTIN, GPIO_OUT);
 
-    // Flash LED
-    utils::flash_led(LED_BUILTIN);
+    for (int i = 0; i < 3; i++) {
+        gpio_put(LED_BUILTIN, 1);  // Set pin 25 to high
+        printf("LED ON!\n");
+        sleep_ms(1000);  // 0.5s delay
+
+        gpio_put(LED_BUILTIN, 0);  // Set pin 25 to low
+        printf("LED OFF!\n");
+        sleep_ms(1000);  // 0.5s delay
+    }
+
+    printf("Setting up load cell\n");
+    hx711_t hx;
+    hx711_init(&hx, CLKPIN, DATPIN, pio0, &hx711_noblock_program,
+               &hx711_noblock_program_init);
+    hx711_power_up(&hx, hx711_gain_128);
+    hx711_wait_settle(hx711_rate_10);
 
     // pwm config
     // Note that PWM needs to be setup first,
@@ -133,6 +155,13 @@ int main() {
     printf("Initial throttle: %i", shoot::throttle_code);
     printf("\tInitial telemetry: %i", shoot::telemetry);
 
-    while (1) loop();
+    while (1) {
+        key_input = getchar_timeout_us(0);
+        update_signal(key_input);
+        sleep_ms(100);
+        if (hx711_get_value_noblock(&hx, &thrust)) {
+            printf("%li\n", thrust - zero_val);
+        }
+    }
 }
 
