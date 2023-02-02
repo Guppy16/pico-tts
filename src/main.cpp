24e83afb2576e15f7fa493790f5abae323334a5b
Akash Gupta <akash.gupta@cambridgeconsultants.com>
2023-02-02 22:12:34 +0000

    fix: setting throttle codes using MIN and MAX in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,107 +8,107 @@
 #include "utils.h"
 
 void update_signal(int &key_input) {
-    // l (led)
-    if (key_input == 108) {
-        utils::flash_led(LED_BUILTIN);
-    }
+  // l (led)
+  if (key_input == 108) {
+    utils::flash_led(LED_BUILTIN);
+  }
 
-    // b - beep
-    if (key_input == 98) {
-        shoot::throttle_code = 1;
-        shoot::telemetry = 1;
-    }
+  // b - beep
+  if (key_input == 98) {
+    shoot::throttle_code = 1;
+    shoot::telemetry = 1;
+  }
 
-    // r - rise
-    if (key_input == 114) {
-        if (shoot::throttle_code >= ZERO_THROTTLE and
-            shoot::throttle_code <= MAX_THROTTLE) {
-            // Check for max throttle
-            if (shoot::throttle_code == MAX_THROTTLE) {
-                printf("Max Throttle reached\n");
-            } else {
-                shoot::throttle_code = MIN(
-                    shoot::throttle_code + THROTTLE_INCREMENT, MAX_THROTTLE);
-                if (shoot::throttle_code > MAX_THROTTLE)
-                    shoot::throttle_code = MAX_THROTTLE;
-                printf("Throttle: %i\n", shoot::throttle_code - ZERO_THROTTLE);
-            }
-        } else {
-            printf("Motor is not in throttle mode\n");
-        }
-    }
+  // r - rise
+  if (key_input == 114) {
+    if (shoot::throttle_code >= ZERO_THROTTLE and
+        shoot::throttle_code <= MAX_THROTTLE) {
 
-    // f - fall
-    if (key_input == 102) {
-        if (shoot::throttle_code <= MAX_THROTTLE &&
-            shoot::throttle_code >= ZERO_THROTTLE) {
-            if (shoot::throttle_code == ZERO_THROTTLE) {
-                printf("Throttle is zero\n");
-            } else {
-                shoot::throttle_code = MIN(
-                    shoot::throttle_code - THROTTLE_INCREMENT, ZERO_THROTTLE);
-                printf("Throttle: %i\n", shoot::throttle_code - ZERO_THROTTLE);
-            }
-        } else {
-            printf("Motor is not in throttle mode\n");
-        }
+      shoot::throttle_code =
+          MIN(shoot::throttle_code + THROTTLE_INCREMENT, MAX_THROTTLE);
+      // Check for max throttle
+      if (shoot::throttle_code == MAX_THROTTLE) {
+        printf("Max Throttle reached\n");
+      } else {
+        printf("Throttle: %i\n", shoot::throttle_code - ZERO_THROTTLE);
+      }
+    } else {
+      printf("Motor is not in throttle mode\n");
     }
+  }
 
-    // q - send zero throttle
-    if (key_input == 32) {
-        shoot::throttle_code = ZERO_THROTTLE;
-        shoot::telemetry = 0;
-        printf("Throttle: %i\n", 0);
+  // f - fall
+  if (key_input == 102) {
+    if (shoot::throttle_code <= MAX_THROTTLE &&
+        shoot::throttle_code >= ZERO_THROTTLE) {
+
+      shoot::throttle_code =
+          MAX(shoot::throttle_code - THROTTLE_INCREMENT, ZERO_THROTTLE);
+			// Check if min throttle reached
+      if (shoot::throttle_code == ZERO_THROTTLE) {
+        printf("Throttle is zero\n");
+      } else {
+        printf("Throttle: %i\n", shoot::throttle_code - ZERO_THROTTLE);
+      }
+    } else {
+      printf("Motor is not in throttle mode\n");
     }
+  }
 
-    shoot::send_dshot_frame();
-    printf("Finished processing byte.\n");
+  // q - send zero throttle
+  if (key_input == 32) {
+    shoot::throttle_code = ZERO_THROTTLE;
+    shoot::telemetry = 0;
+    printf("Throttle: %i\n", 0);
+  }
+
+  shoot::send_dshot_frame();
+  printf("Finished processing byte.\n");
 }
 
 int main() {
-    int32_t thrust;
-    int key_input;
+  int32_t thrust;
+  int key_input;
 
-    // Load cell calibration
-    int32_t zero_val = 150622;
+  // Load cell calibration
+  int32_t zero_val = 150622;
 
-    stdio_init_all();
-    gpio_init(LED_BUILTIN);
-    gpio_set_dir(LED_BUILTIN, GPIO_OUT);
+  stdio_init_all();
+  gpio_init(LED_BUILTIN);
+  gpio_set_dir(LED_BUILTIN, GPIO_OUT);
 
-    // Flash LED on and off 3 times
-    for (int i = 0; i < 3; i++) {
-        utils::flash_led(LED_BUILTIN);
+  // Flash LED on and off 3 times
+  for (int i = 0; i < 3; i++) {
+    utils::flash_led(LED_BUILTIN);
+  }
+
+  sleep_ms(1500);
+
+  printf("Setting up load cell\n");
+  hx711_t hx;
+  hx711_init(&hx, CLKPIN, DATPIN, pio0, &hx711_noblock_program,
+             &hx711_noblock_program_init);
+  hx711_power_up(&hx, hx711_gain_128);
+  hx711_wait_settle(hx711_rate_10);
+
+  printf("Setting up dshot\n");
+  tts::pwm_setup();
+  tts::dma_setup();
+  shoot::rt_setup();
+
+  // tts::print_gpio_setup();
+  // tts::print_dshot_setup();
+  // tts::print_dma_setup();
+
+  printf("Initial throttle:\n %i\n", shoot::throttle_code);
+  printf("Initial telemetry:\n %i\n", shoot::telemetry);
+
+  while (1) {
+    key_input = getchar_timeout_us(0);
+    update_signal(key_input);
+    sleep_ms(100);
+    if (hx711_get_value_noblock(&hx, &thrust)) {
+      printf("%li\n", thrust - zero_val);
     }
-
-    sleep_ms(1500);
-
-    printf("Setting up load cell\n");
-    hx711_t hx;
-    hx711_init(&hx, CLKPIN, DATPIN, pio0, &hx711_noblock_program,
-               &hx711_noblock_program_init);
-    hx711_power_up(&hx, hx711_gain_128);
-    hx711_wait_settle(hx711_rate_10);
-
-    printf("Setting up dshot\n");
-    tts::pwm_setup();
-    tts::dma_setup();
-    shoot::rt_setup();
-
-    // tts::print_gpio_setup();
-    // tts::print_dshot_setup();
-    // tts::print_dma_setup();
-
-    printf("Initial throttle:\n %i\n", shoot::throttle_code);
-    printf("Initial telemetry:\n %i\n", shoot::telemetry);
-
-    while (1) {
-        key_input = getchar_timeout_us(0);
-        update_signal(key_input);
-        sleep_ms(100);
-        if (hx711_get_value_noblock(&hx, &thrust)) {
-            printf("%li\n", thrust - zero_val);
-        }
-    }
+  }
 }
-
