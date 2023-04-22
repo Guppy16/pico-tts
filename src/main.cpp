9eb40de15619a15033cdfdd0629f3b7fb43dd72c
Akash <ag2118@cam.ac.uk>
2023-04-22 17:35:35 +0100

    refactor: use pico-dshot library

--- src/main.cpp
+++ src/main.cpp
@@ -1,122 +1,158 @@
-#include <stdio.h>
-#include <string.h>
+/**
+ * @file main.cpp
+ * @brief send dshot commands to ESC and measure throttle using HX711
+ *
+ * This file was adapted from pico-dshot/examples/keyboard_control.
+ * We send commands from the keyboard to command the motor via an ESC.
+ * The throttle is measure using a HX711.
+ */
 
+// #include "stdio.h"
+// #include <string.h>
+
+#include "dshot.h"
 #include "hx711.h"
 #include "hx711_noblock.pio.h"
 #include "pico/platform.h"
 #include "pico/stdlib.h"
-#include "shoot.h"
 #include "utils.h"
 
-void update_signal(const int &key_input) {
-    // l (led)
-    if (key_input == 108) {
-        utils::flash_led(LED_BUILTIN);
+#define LED_BUILTIN 25
+
+// dshot ESC config
+constexpr uint esc_gpio = 14;
+constexpr float dshot_speed = 1200.0f;
+constexpr int64_t packet_interval_us = 1000 / 7; // 7 kHz packet freq
+constexpr uint16_t throttle_increment = 50;
+
+// hx711 config
+constexpr uint8_t HX711_CLKPIN = 17;
+constexpr uint8_t HX711_DATPIN = 16;
+
+/**
+ * @brief Map key input to a dshot command
+ *
+ * @param key_input keyboard input
+ * @param dshot dshot_config to set the throttle code and telemetry
+ * depending on the key input
+ * @return true
+ * @return false Not implemented
+ *
+ * @attention
+ * Key presses are stored in a buffer in the pico,
+ * hence spamming a key will cause downstream keys
+ * to be executed later than expected.
+ */
+bool update_signal(const int &key_input, dshot_config &dshot) {
+  printf("Processing key input %i\n", key_input);
+
+  switch (key_input) {
+  // b - beep
+  case 98:
+    dshot.packet.throttle_code = 1;
+    dshot.packet.telemetry = 1;
+    break;
+
+  // r - rise
+  case 114:
+    // Check if dshot is in throttle mode
+    if (dshot.packet.throttle_code >= DSHOT_ZERO_THROTTLE) {
+      dshot.packet.throttle_code = MIN(
+          dshot.packet.throttle_code + throttle_increment, DSHOT_MAX_THROTTLE);
+      printf("Throttle: %i\n",
+             dshot.packet.throttle_code - DSHOT_ZERO_THROTTLE);
+      if (dshot.packet.throttle_code == DSHOT_MAX_THROTTLE)
+        printf("Max Throttle reached\n");
+    } else {
+      printf("Motor is not in throttle mode\n");
     }
+    break;
 
-    // b - beep
-    if (key_input == 98) {
-        shoot::throttle_code = 1;
-        shoot::telemetry = 1;
+  // f - fall
+  case 102:
+    // Check if dshot is in throttle mode
+    if (dshot.packet.throttle_code >= DSHOT_ZERO_THROTTLE) {
+      dshot.packet.throttle_code = MAX(
+          dshot.packet.throttle_code - throttle_increment, DSHOT_ZERO_THROTTLE);
+      printf("Throttle: %i\n",
+             dshot.packet.throttle_code - DSHOT_ZERO_THROTTLE);
+      if (dshot.packet.throttle_code == DSHOT_ZERO_THROTTLE)
+        printf("Throttle is zero\n");
+    } else {
+      printf("Motor is not in throttle mode\n");
     }
+    break;
 
-    // r - rise
-    if (key_input == 114) {
-        if (shoot::throttle_code >= ZERO_THROTTLE and
-            shoot::throttle_code <= MAX_THROTTLE) {
-            shoot::throttle_code =
-                MIN(shoot::throttle_code + THROTTLE_INCREMENT, MAX_THROTTLE);
+  // spacebar - send zero throttle
+  case 32:
+    dshot.packet.throttle_code = DSHOT_ZERO_THROTTLE;
+    dshot.packet.telemetry = 0;
+    printf("Throttle: 0\n");
+    break;
 
-            printf("Throttle: %i\n", shoot::throttle_code - ZERO_THROTTLE);
-            shoot::throttle_code ==
-                MAX_THROTTLE &&printf("Max Throttle reached\n");
+  // l - led: flash led on pico to check it is responsive
+  // ironically, this is a blocking process
+  case 108:
+    utils::flash_led(LED_BUILTIN);
+    break;
 
-        } else {
-            printf("Motor is not in throttle mode\n");
-        }
-    }
+  default:
+    printf("Key is not registered with a command\n");
+  }
 
-    // f - fall
-    if (key_input == 102) {
-        if (shoot::throttle_code <= MAX_THROTTLE &&
-            shoot::throttle_code >= ZERO_THROTTLE) {
-            shoot::throttle_code =
-                MAX(shoot::throttle_code - THROTTLE_INCREMENT, ZERO_THROTTLE);
+  printf("Finished processing key input\n");
 
-            printf("Throttle: %i\n", shoot::throttle_code - ZERO_THROTTLE);
-            shoot::throttle_code ==
-                ZERO_THROTTLE &&printf("Throttle is zero\n");
-
-        } else {
-            printf("Motor is not in throttle mode\n");
-        }
-    }
-
-    // spacebar - send zero throttle
-    if (key_input == 32) {
-        shoot::throttle_code = ZERO_THROTTLE;
-        shoot::telemetry = 0;
-        printf("Throttle: %i\n", 0);
-    }
-
-    shoot::send_dshot_frame();
-    printf("Finished processing key input %i\n", key_input);
+  return true;
 }
 
 void print_load_cell_setup() {
-    printf("\nHX711 Load Cell Setup\n");
-    printf("Pins: CLK %i DAT %i\n", HX711_CLKPIN, HX711_DATPIN);
+  printf("\nHX711 Load Cell Setup\n");
+  printf("Pins: CLK %i DAT %i\n", HX711_CLKPIN, HX711_DATPIN);
 }
 
 int main() {
-    // Set MCU clock frequency. Should we assert this?
-    set_sys_clock_khz(MCU_FREQ * 1e3, false);
+  stdio_init_all();
 
-    int32_t thrust;
-    int key_input;
+  // --- Setup builtin led
+  gpio_init(LED_BUILTIN);
+  gpio_set_dir(LED_BUILTIN, GPIO_OUT);
 
-    stdio_init_all();
-    gpio_init(LED_BUILTIN);
-    gpio_set_dir(LED_BUILTIN, GPIO_OUT);
+  // flash led to wait a few seconds for serial uart to setup
+  // also to check if the pico is responsive
+  utils::flash_led(LED_BUILTIN, 3);
 
-    // Flash LED on and off 3 times
-    utils::flash_led(LED_BUILTIN, 3);
+  // --- Setup Load cell
+  int32_t zero_val = 150622; // Load cell calibration
+  hx711_t hx;
+  hx711_init(&hx, HX711_CLKPIN, HX711_DATPIN, pio0, &hx711_noblock_program,
+             &hx711_noblock_program_init);
+  hx711_power_up(&hx, hx711_gain_128);
+  hx711_wait_settle(hx711_rate_10);
+  print_load_cell_setup();
 
-    // Setup Load cell
-    int32_t zero_val = 150622;  // Load cell calibration
-    hx711_t hx;
-    hx711_init(&hx, HX711_CLKPIN, HX711_DATPIN, pio0, &hx711_noblock_program,
-               &hx711_noblock_program_init);
-    hx711_power_up(&hx, hx711_gain_128);
-    hx711_wait_settle(hx711_rate_10);
+  // --- setup dshot
+  // Get the default alarm pool. This is passed in to the dshot init,
+  // so that we can setup a repeating timer to send dshot packets
+  alarm_pool_t *pico_alarm_pool = alarm_pool_get_default();
 
-    // Setup DShot
-    tts::pwm_setup();
-    tts::dma_setup();
-    shoot::rt_setup();
+  // initialise dshot config
+  dshot_config dshot;
+  dshot_config_init(&dshot, dshot_speed, esc_gpio, packet_interval_us,
+                    pico_alarm_pool);
+  print_dshot_config(&dshot);
 
-    sleep_ms(1500);
+  // --- main loop
+  int32_t thrust; // Measured thrust from hx711
+  int key_input;  // Keyboard input to command the motor
+  while (1) {
+    // Read key input from keyboard
+    key_input = getchar_timeout_us(0);
+    key_input != PICO_ERROR_TIMEOUT &&update_signal(key_input, dshot);
 
-    printf("MCU Freq (kHz): Expected %d Actual %d\n", MCU_FREQ * 1000,
-           frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_SYS));
-
-    tts::print_gpio_setup();
-    tts::print_dshot_setup();
-    tts::print_dma_setup();
-    shoot::print_rt_setup();
-    print_load_cell_setup();
-
-    printf("Initial throttle: %i\n", shoot::throttle_code);
-    printf("Initial telemetry: %i\n", shoot::telemetry);
-
-    while (1) {
-        key_input = getchar_timeout_us(0);
-        if (key_input != PICO_ERROR_TIMEOUT) {
-            update_signal(key_input);
-        }
-        sleep_ms(100);
-        if (hx711_get_value_noblock(&hx, &thrust) && thrust) {
-            printf("hx711: %li\n", thrust - zero_val);
-        }
+    // Read from hx711
+    sleep_ms(100);
+    if (hx711_get_value_noblock(&hx, &thrust) && thrust) {
+      printf("hx711: %li\n", thrust - zero_val);
     }
+  }
 }
